1229a4229bb1a0722c8280b39f4e80275e6b1029
Noah Anabiik Schwarz <noah.anabiik.schwarz@gmail.com>
2019-08-18 11:16:48 +0200

    better constant time equal

--- utils.ts
+++ utils.ts
@@ -1,26 +1,15 @@
-/**
- * Time constant comparison of two arrays
- * @param {Uint8Array} lh First array of bytes
- * @param {Uint8Array} rh Second array of bytes
- * @return {Boolean} True if the arrays are equal (length and content), false otherwise
- */
-export function compare(lh: Uint8Array, rh: Uint8Array): boolean {
-  if (lh.length !== rh.length) {
-    // abort
-    return false;
+/** Hopefully a somewhat timing-attack-robust buffer equality check. */
+export function compare(a: Uint8Array, b: Uint8Array): boolean {
+  let diff: number = a.length === b.length ? 0 : 1;
+
+  for (let i: number = Math.max(a.length, b.length) - 1; i >= 0; --i) {
+    diff |= a[i] ^ b[i];
   }
-  let i, d = 0, len = lh.length;
-  for (i = 0; i < len; i++) {
-    d |= lh[i] ^ rh[i];
-  }
-  return d === 0;
+
+  return diff === 0;
 }
 
-/**
- * Convert a hex string to byte array
- * @param {String} hex Hex string
- * @return {Uint8Array} Byte array
- */
+/** Converts a hex string to a byte array. */
 export function hex2bin(hex: string): Uint8Array {
   if (hex.indexOf('0x') === 0 || hex.indexOf('0X') === 0) {
     hex = hex.substr(2);
